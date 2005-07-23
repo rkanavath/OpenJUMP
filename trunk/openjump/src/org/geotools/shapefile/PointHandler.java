06e91065b3beca5af6beee7eeffd1eb4fe776de3
javamap <javamap@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2005-07-23 22:57:09 +0000

    modified to implement SIGLE function of cleaning memory

--- trunk/openjump/src/org/geotools/shapefile/PointHandler.java
+++ trunk/openjump/src/org/geotools/shapefile/PointHandler.java
@@ -7,8 +7,11 @@ import com.vividsolutions.jump.io.EndianDataInputStream;
 import com.vividsolutions.jump.io.EndianDataOutputStream;
 /**
  * Wrapper for a Shapefile point.
+ * 
+ * getLength() modified by Michaël MICHAUD on 3 nov. 2004 to handle properly
+ * Point, PointM and PointZ length
  */
-public class PointHandler implements ShapeHandler{
+public class PointHandler implements ShapeHandler {
     
     int Ncoords=2; //2 = x,y ;  3= x,y,m ; 4 = x,y,z,m
     int myShapeType = -1;
@@ -40,7 +43,7 @@ public class PointHandler implements ShapeHandler{
         double y = file.readDoubleLE();
         double m , z = Double.NaN;
 		actualReadWords += 8;
-        
+        /*
         if ( shapeType ==11 )
         {
             z= file.readDoubleLE();
@@ -51,6 +54,22 @@ public class PointHandler implements ShapeHandler{
             m = file.readDoubleLE();
 			actualReadWords += 4;
         }
+        */
+        // added on march, 24 by michael michaud
+        // to read shapefile containing PointZ without m value
+        if ( shapeType ==21 ) {
+            m= file.readDoubleLE();
+            actualReadWords += 4;
+        }
+        else if ( shapeType ==11 ) {
+            z = file.readDoubleLE();
+            actualReadWords += 4;
+            if (contentLength>actualReadWords) {
+                m = file.readDoubleLE();
+                actualReadWords += 8;
+            }
+        }
+
         
 	//verify that we have read everything we need
 	while (actualReadWords < contentLength)
@@ -95,11 +114,13 @@ public class PointHandler implements ShapeHandler{
      * @return int The length of the record that this shapepoint will take up in a shapefile
      **/
     public int getLength(Geometry geometry){
-        if (myShapeType == 21)
+        // 2004/03/12 : type 1 (Point) has length 10
+        if (myShapeType == 1)
             return 10;
-        if (myShapeType == 11)
+        // 2004/03/12 : type 21 (PointM) has length 14
+        if (myShapeType == 21)
             return 14;
-        
+        // 2004/03/12 : type 11 (PointZ) has length 18
         return 18;
     }
 }
