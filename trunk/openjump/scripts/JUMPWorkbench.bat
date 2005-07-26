47747727ad17e3b4ba8d85d1085a22ee7e8ad854
jonathanaquino <jonathanaquino@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2005-07-26 08:25:38 +0000

    Fixed Ant script

--- trunk/openjump/scripts/JUMPWorkbench.bat
+++ trunk/openjump/scripts/JUMPWorkbench.bat
@@ -1,5 +1,5 @@
 set LIB=../lib
-set CLASSPATH=%LIB%/bsh-2.0b1.jar;%LIB%/Jama-1.0.1.jar;%LIB%/jdom.jar;%LIB%/jts-1.4.1-RC1.jar;%LIB%/jump-workbench-@VERSION@.jar;%LIB%/jump-api-@VERSION@.jar;%LIB%/xercesImpl.jar;%LIB%/xml-apis.jar
+set CLASSPATH=%LIB%/bsh-2.0b4.jar;%LIB%/Jama-1.0.1.jar;%LIB%/jdom.jar;%LIB%/jts-1.6.jar;%LIB%/jump-workbench-@VERSION@.jar;%LIB%/jump-api-@VERSION@.jar;%LIB%/xercesImpl.jar;%LIB%/xml-apis.jar;%LIB%/log4j-1.2.8.jar;%LIB%/looks-1.3.1.jar
 REM Add extension directory to path, so extensions can put DLL's there [Jon Aquino 2005-03-18]
 set PATH=%PATH%;%LIB%/ext
-start javaw -Xms256M -Xmx256M -cp %CLASSPATH% com.vividsolutions.jump.workbench.JUMPWorkbench -properties workbench-properties.xml -plug-in-directory %LIB%/ext
+start javaw -Dlog4j.configuration=file:./log4j.xml -Xms256M -Xmx256M -cp %CLASSPATH% com.vividsolutions.jump.workbench.JUMPWorkbench -properties workbench-properties.xml -plug-in-directory %LIB%/ext
