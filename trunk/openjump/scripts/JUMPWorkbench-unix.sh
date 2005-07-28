a911cf4e49a56ecea84a19c4a85577e8cc4adba5
jonathanaquino <jonathanaquino@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2005-07-28 07:39:23 +0000

    Drop reference to looks.jar from scripts.

--- trunk/openjump/scripts/JUMPWorkbench-unix.sh
+++ trunk/openjump/scripts/JUMPWorkbench-unix.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #To make this script executable, use chmod a+x JUMPWorkbench-unix.sh
 LIB=../lib
-CLASSPATH=$LIB/bsh-2.0b4.jar:$LIB/Jama-1.0.1.jar:$LIB/jdom.jar:$LIB/jts-1.6.jar:$LIB/jump-workbench-@VERSION@.jar:$LIB/jump-api-@VERSION@.jar:$LIB/xercesImpl.jar:$LIB/xml-apis.jar:$LIB/log4j-1.2.8.jar:$LIB/looks-1.3.1.jar
+CLASSPATH=$LIB/bsh-2.0b4.jar:$LIB/Jama-1.0.1.jar:$LIB/jdom.jar:$LIB/jts-1.6.jar:$LIB/jump-workbench-@VERSION@.jar:$LIB/jump-api-@VERSION@.jar:$LIB/xercesImpl.jar:$LIB/xml-apis.jar:$LIB/log4j-1.2.8.jar
 java -Dlog4j.configuration=file:./log4j.xml -Xms256M -Xmx256M -cp $CLASSPATH com.vividsolutions.jump.workbench.JUMPWorkbench -properties workbench-properties.xml -plug-in-directory $LIB/ext
