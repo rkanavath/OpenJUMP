fa89628941dd3b12068616670a238153c179e048
taddei <taddei@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2005-10-18 12:15:58 +0000

    added field and accessor; changed constructor too

--- trunk/openjump/src/com/vividsolutions/jump/workbench/model/FeatureEvent.java
+++ trunk/openjump/src/com/vividsolutions/jump/workbench/model/FeatureEvent.java
@@ -45,6 +45,10 @@ public class FeatureEvent {
     private FeatureEventType type;
     private Collection features;
     private Collection oldFeatureClones;
+    
+    //[UT] 25.08.2005 added 
+    private Collection oldFeatureAttClones;
+    
     /**
      * @param oldFeatureClones for GEOMETRY_MODIFIED events, clones of the Features before
      * they were modified; null for other events
@@ -56,13 +60,24 @@ public class FeatureEvent {
         Collection oldFeatureClones) {
         Assert.isTrue(layer != null);
         Assert.isTrue(type != null);
+        
         Assert.isTrue(
             (type == FeatureEventType.GEOMETRY_MODIFIED && oldFeatureClones != null)
-                || (type != FeatureEventType.GEOMETRY_MODIFIED && oldFeatureClones == null));
+                || (type != FeatureEventType.GEOMETRY_MODIFIED && oldFeatureClones == null)
+                || (type == FeatureEventType.ATTRIBUTES_MODIFIED && oldFeatureAttClones == null ) );
+        
         this.layer = layer;
         this.type = type;
         this.features = features;
-        this.oldFeatureClones = oldFeatureClones;
+
+        //      [UT] 25.08.2005 uncommented and...
+        //      this.oldFeatureClones = oldFeatureClones;
+        //[UT] 25.08.2005 did like this
+        if( this.type == FeatureEventType.GEOMETRY_MODIFIED){
+            this.oldFeatureClones = oldFeatureClones;
+        } else if( this.type == FeatureEventType.ATTRIBUTES_MODIFIED){
+            this.oldFeatureAttClones = oldFeatureClones;
+        }
     }
     public Layer getLayer() {
         return layer;
@@ -75,5 +90,10 @@ public class FeatureEvent {
     }
     public Collection getOldFeatureClones() {
         return Collections.unmodifiableCollection(oldFeatureClones);
+    }
+    
+    //[UT] 25.08.2005 added 
+    public Collection getOldFeatureAttClones() {
+        return Collections.unmodifiableCollection(oldFeatureAttClones);
     }
 }
