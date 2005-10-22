d58fd95b544cc984785beff4e5c7a0ef1caedccf
michaudm <michaudm@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2005-10-22 12:30:11 +0000

    Version 0.2 of SimpleQueryPlugIn

--- trunk/openjump/src/org/openjump/core/ui/plugin/queries/SimpleQueryPlugIn.java
+++ trunk/openjump/src/org/openjump/core/ui/plugin/queries/SimpleQueryPlugIn.java
@@ -8,12 +8,20 @@ import org.openjump.core.ui.plugin.queries.SimpleQueryDialog;
 import com.vividsolutions.jump.I18N;
 
 /**
- * SimpleQueryPlugIn
+ * SimpleQueryPlugIn is a query editor and processor.
+ * It has the following capabilities :
+ * <OL><LI>query one or more layers</LI>
+ * <LI>attribute queries and spatial queries</LI>
+ * <LI>numerical and string functions</LI>
+ * <LI>regular expression to find strings</LI>
+ * <LI>results as a selection, a table or a new layer</LI></OL>
+ * Version 0.2 of the SimpleQueryPlugIn is an adaptation of the original
+ * version to the core of OpenJUMP (refactoring, internationalization)
  * @author Michaël MICHAUD
- * @version 0.1.0 (4 Dec 2004)
+ * @version 0.2 (16 Oct 2005)
  */ 
 public class SimpleQueryPlugIn extends AbstractPlugIn {
-    static SimpleQueryDialog simplequeryDialog;
+    static QueryDialog queryDialog;
 	
     public void initialize(PlugInContext context) throws Exception {
 
@@ -25,10 +33,10 @@ public class SimpleQueryPlugIn extends AbstractPlugIn {
     }
                                                       
     public boolean execute(PlugInContext context) throws Exception {
-        if (simplequeryDialog==null) {
-            simplequeryDialog = new SimpleQueryDialog(context);
+        if (queryDialog==null) {
+            queryDialog = new QueryDialog(context);
         }
-        simplequeryDialog.initUI();
+        //queryDialog.initUI();
         return false;
     }
     
