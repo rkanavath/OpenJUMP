41eaf97bba549435c23815b79fde64720a9c3958
michaudm <michaudm@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2005-10-22 12:29:14 +0000

    Add the Buoy.jar library in the classpath for SimpleQueryPlugIn

--- trunk/openjump/scripts/JUMPWorkbench-mac.sh
+++ trunk/openjump/scripts/JUMPWorkbench-mac.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 LIB=../lib
-CLASSPATH=$LIB/bsh-2.0b4.jar:$LIB/Jama-1.0.1.jar:$LIB/jdom.jar:$LIB/jts-1.6.jar:$LIB/jump-workbench-@VERSION@.jar:$LIB/jump-api-@VERSION@.jar:$LIB/xercesImpl.jar:$LIB/xml-apis.jar:$LIB/log4j-1.2.8.jar:$LIB/batik-awt-util.jar:$LIB/batik-dom.jar:$LIB/batik-svggen.jar:$LIB/batik-util.jar:$LIB/batik-xml.jar
+CLASSPATH=$LIB/bsh-2.0b4.jar:$LIB/Buoy.jar:$LIB/Jama-1.0.1.jar:$LIB/jdom.jar:$LIB/jts-1.6.jar:$LIB/jump-workbench-@VERSION@.jar:$LIB/jump-api-@VERSION@.jar:$LIB/xercesImpl.jar:$LIB/xml-apis.jar:$LIB/log4j-1.2.8.jar:$LIB/batik-awt-util.jar:$LIB/batik-dom.jar:$LIB/batik-svggen.jar:$LIB/batik-util.jar:$LIB/batik-xml.jar
 java -Dlog4j.configuration=file:./log4j.xml -Xdock:name=JUMP -Xms256M -Xmx256M -cp $CLASSPATH com.vividsolutions.jump.workbench.JUMPWorkbench -properties workbench-properties.xml -plug-in-directory $LIB/ext
