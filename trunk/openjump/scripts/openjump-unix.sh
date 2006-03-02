4333e7314e737321236c0b51d809e465d5b87f51
mentaer <mentaer@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2006-03-02 16:06:45 +0000

    update jump code to openjump code: jts 1.7 needed for spatial query (1.6 deleted)

--- trunk/openjump/scripts/openjump-unix.sh
+++ trunk/openjump/scripts/openjump-unix.sh
@@ -9,6 +9,6 @@
 
 # The next lines are required in any case
 LIB=../lib
-CLASSPATH=$LIB/bsh-2.0b4.jar:$LIB/Buoy.jar:$LIB/Jama-1.0.1.jar:$LIB/jdom.jar:$LIB/jts-1.6.jar:$LIB/jump-workbench-@VERSION@.jar:$LIB/jump-api-@VERSION@.jar:$LIB/xercesImpl.jar:$LIB/xml-apis.jar:$LIB/log4j-1.2.8.jar:$LIB/batik/batik-awt-util.jar:$LIB/batik/batik-dom.jar:$LIB/batik/batik-svggen.jar:$LIB/batik/batik-util.jar:$LIB/batik/batik-xml.jar
+CLASSPATH=$LIB/bsh-2.0b4.jar:$LIB/Buoy.jar:$LIB/Jama-1.0.1.jar:$LIB/jdom.jar:$LIB/jts-1.7.jar:$LIB/jump-workbench-@VERSION@.jar:$LIB/jump-api-@VERSION@.jar:$LIB/xercesImpl.jar:$LIB/xml-apis.jar:$LIB/log4j-1.2.8.jar:$LIB/batik/batik-awt-util.jar:$LIB/batik/batik-dom.jar:$LIB/batik/batik-svggen.jar:$LIB/batik/batik-util.jar:$LIB/batik/batik-xml.jar
 java -Dswing.defaultlaf=javax.swing.plaf.metal.MetalLookAndFeel -Dlog4j.configuration=file:./log4j.xml -Xms256M -Xmx256M -cp $CLASSPATH com.vividsolutions.jump.workbench.JUMPWorkbench -properties workbench-properties.xml -plug-in-directory $LIB/ext
 
