4bc0d25fdda3debae37f14672473cd309d25b375
mentaer <mentaer@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2006-07-15 15:56:32 +0000

    changed to add jmat.jar

--- trunk/openjump/scripts/openjump.bat
+++ trunk/openjump/scripts/openjump.bat
@@ -1,5 +1,5 @@
 set LIB=../lib
-set CLASSPATH=%LIB%/bsh-2.0b4.jar;%LIB%/Buoy.jar;%LIB%/Jama-1.0.1.jar;%LIB%/jdom.jar;%LIB%/jts-1.7.jar;%LIB%/jump-workbench-@VERSION@.jar;%LIB%/jump-api-@VERSION@.jar;%LIB%/xercesImpl.jar;%LIB%/xml-apis.jar;%LIB%/log4j-1.2.8.jar;%LIB%/batik/batik-awt-util.jar;%LIB%/batik/batik-dom.jar;%LIB%/batik/batik-svggen.jar;%LIB%/batik/batik-util.jar;%LIB%/batik/batik-xml.jar
+set CLASSPATH=%LIB%/bsh-2.0b4.jar;%LIB%/Buoy.jar;%LIB%/Jama-1.0.1.jar;%LIB%/jdom.jar;%LIB%/jts-1.7.jar;%LIB%/jump-workbench-@VERSION@.jar;%LIB%/jump-api-@VERSION@.jar;%LIB%/xercesImpl.jar;%LIB%/xml-apis.jar;%LIB%/log4j-1.2.8.jar;%LIB%/batik/batik-awt-util.jar;%LIB%/batik/batik-dom.jar;%LIB%/batik/batik-svggen.jar;%LIB%/batik/batik-util.jar;%LIB%/batik/batik-xml.jar; %LIB%/jmat_5.0.jar
 REM Add extension directory to path, so extensions can put DLL's there [Jon Aquino 2005-03-18]
 set PATH=%PATH%;%LIB%/ext
 start javaw -Dlog4j.configuration=file:./log4j.xml -Xms256M -Xmx256M -cp %CLASSPATH% com.vividsolutions.jump.workbench.JUMPWorkbench -properties workbench-properties.xml -plug-in-directory %LIB%/ext
