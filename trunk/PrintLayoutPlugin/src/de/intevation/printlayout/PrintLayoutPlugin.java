ce2a7ca6098711d78dc650e292dc0716530e1a2f
s-l-teichmann <s-l-teichmann@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2007-01-12 12:55:30 +0000

    *** empty log message ***

---
+++ trunk/PrintLayoutPlugin/src/de/intevation/printlayout/PrintLayoutPlugin.java
@@ -0,0 +1,45 @@
+package de.intevation.printlayout;
+
+import com.vividsolutions.jump.workbench.plugin.PlugInContext;
+import com.vividsolutions.jump.workbench.plugin.AbstractPlugIn;
+import com.vividsolutions.jump.workbench.plugin.ThreadedPlugIn;
+import com.vividsolutions.jump.workbench.plugin.EnableCheckFactory;
+
+import com.vividsolutions.jump.util.Blackboard;
+
+import com.vividsolutions.jump.task.TaskMonitor;  
+
+public class PrintLayoutPlugin 
+extends      AbstractPlugIn 
+implements   ThreadedPlugIn
+{
+	protected Blackboard blackboard;
+
+	public void initialize(PlugInContext context)
+	throws Exception
+	{
+		EnableCheckFactory check = 
+		  new EnableCheckFactory(context.getWorkbenchContext());
+
+		context
+			.getFeatureInstaller()
+			.addMainMenuItem(
+				this, new String[] { "File" }, getName(),
+				false, null, check.createAtLeastNLayersMustExistCheck(1));
+
+		 blackboard = new Blackboard();
+	}
+   
+
+	public boolean execute(PlugInContext context)
+	throws Exception
+	{
+		return true;
+	}
+
+	public void run(TaskMonitor monitor, PlugInContext context)
+	{
+		monitor.allowCancellationRequests();
+	}
+}		
+// end of file
