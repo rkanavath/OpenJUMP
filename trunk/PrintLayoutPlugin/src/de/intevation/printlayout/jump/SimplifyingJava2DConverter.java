540471f985e6e07a2e435f96d22b15f0b335bf33
s-l-teichmann <s-l-teichmann@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2007-03-21 08:56:48 +0000

    Added a system property to decide if polygons are simplified with guaranteed preserving the topology.

--- trunk/PrintLayoutPlugin/src/de/intevation/printlayout/jump/SimplifyingJava2DConverter.java
+++ trunk/PrintLayoutPlugin/src/de/intevation/printlayout/jump/SimplifyingJava2DConverter.java
@@ -20,11 +20,26 @@ import com.vividsolutions.jts.simplify.TopologyPreservingSimplifier;
 import java.awt.Shape;
 
 import java.awt.geom.NoninvertibleTransformException;
+import java.awt.geom.Point2D;
 
 public class SimplifyingJava2DConverter
 extends      PreciseJava2DConverter
 {
+	/**
+	 * The tolerance for the simplifications.
+	 */
 	protected double tolerance;
+
+	/**
+	 * If the system property 
+	 * "de.intevation.printlayout.simplify.preserve.topology" is set to
+	 * true then polygons are simplified with guaranteed preserving
+	 * the topology. This is slow and in most case it should be okay
+	 * to simplify the shell and the holes with the faster 
+	 * Douglas Peucker line simplifier. This is the default.
+	 */
+	public static final boolean PRESERVE_TOPOLOGY =
+		Boolean.getBoolean("de.intevation.printlayout.simplify.preserve.topology");
 
 	public SimplifyingJava2DConverter(
 		PointConverter converter, 
@@ -37,8 +52,25 @@ extends      PreciseJava2DConverter
   protected Shape toPreciseShape(Polygon p) 
 	throws NoninvertibleTransformException 
 	{
-		return super.toPreciseShape(
-			(Polygon)TopologyPreservingSimplifier.simplify(p, tolerance));
+		if (PRESERVE_TOPOLOGY)
+			return super.toPreciseShape(
+				(Polygon)TopologyPreservingSimplifier.simplify(p, tolerance));
+
+		Point2D [][] holesCoordinates;
+
+		holesCoordinates = new Point2D[p.getNumInteriorRing()][];
+
+		for (int i = 0; i < holesCoordinates.length; ++i)
+			holesCoordinates[i] =
+				toViewPoints(
+					DouglasPeuckerLineSimplifier.simplify(
+						p.getInteriorRingN(i).getCoordinates(), tolerance));
+
+		return new PrecisePolygonShape(
+			toViewPoints(
+				DouglasPeuckerLineSimplifier.simplify(
+					p.getExteriorRing().getCoordinates(), tolerance)),
+			holesCoordinates);
 	}
 
 	protected Shape toPreciseShape(LineString lineString)
