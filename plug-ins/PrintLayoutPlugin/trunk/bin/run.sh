0b7688a3eafaafe588b574baacb700019eacdd6f
s-l-teichmann <s-l-teichmann@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2007-06-22 11:33:26 +0000

    Adjusted bin/run.sh for SVN infrastructure.

--- plug-ins/PrintLayoutPlugin/trunk/bin/run.sh
+++ plug-ins/PrintLayoutPlugin/trunk/bin/run.sh
@@ -6,12 +6,12 @@ WHAT=${WHAT:-com.vividsolutions.jump.workbench.JUMPWorkbench}
 
 JUMPDIST=${JUMPDIST:-$HOME/prog/openjump/current}
 
-PRINTHOME=${PRINTHOME:-$HOME/prog/PrintLayoutPlugin}
+PRINTHOME=${PRINTHOME:-$HOME/prog/PrintLayoutPlugin-svn}
 
 LOG=$JUMPDIST/bin/log4j.xml
 PROP=$PRINTHOME/etc/workbench-properties.xml
 
-for i in $JUMPDIST/lib/*.jar $PRINTHOME/lib/*.jar ; do 
+for i in $JUMPDIST/lib/*.jar $JUMPDIST/lib/batik/*.jar $PRINTHOME/lib/*.jar ; do 
 	CLASSPATH=$CLASSPATH:$i
 done
 
@@ -21,7 +21,7 @@ shift
 
 java -Dswing.defaultlaf=javax.swing.plaf.metal.MetalLookAndFeel \
      -Dlog4j.configuration=file:$LOG \
-		 -Xms256M -Xmx256M \
+		 -Xms256M -Xmx768M \
 		 -cp $CLASSPATH \
 		 $WHAT \
 		 -properties $PROP \
