36756e8bf90c9a25b225fa8816d67020866ee747
stranger <stranger@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2007-12-18 16:31:03 +0000

    Removed more outdated files.

--- plug-ins/deeJUMP/trunk/scripts/complete_deejumpstart.bat
+++
@@ -1,3 +0,0 @@
--project F:\Programme\eclipse3.0\workspace\elbe-elster\resources\overlay_test.jmp -plug-in-directory "E:/proj/deejump\lib/ext" -properties workbench-state.xml
-
-start javaw.exe  -Duser.language=en -Dlog4j.configuration=file:log4j.xml -jar deejump.jar -properties workbench-properties.xml -plug-in-directory "lib\ext"
