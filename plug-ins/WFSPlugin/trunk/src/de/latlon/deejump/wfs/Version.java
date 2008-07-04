2496a0117ae20d4296d811f2f6548b7a8880184c
sholl <sholl@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2008-07-04 09:37:26 +0000

    prepared to the new release 1.1.0
    	* src/de/latlon/deejump/wfs/Version.java: adopted Version to 1.1.0
    
    	* Changes.txt: added new features of release 1.1.0
    
    	* HowToRelease: adopted Path to version, made it more generic

--- plug-ins/WFSPlugin/trunk/src/de/latlon/deejump/wfs/Version.java
+++ plug-ins/WFSPlugin/trunk/src/de/latlon/deejump/wfs/Version.java
@@ -51,7 +51,7 @@ public class Version {
     /**
      * The current version.
      */
-    public static final String VERSION = "1.2.0-dev";
+    public static final String VERSION = "1.1.0";
 
     /**
      * @param args
