3c6aee647aefcbf033ec3eb462c0665b1326c038
mentaer <mentaer@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2012-11-01 22:52:45 +0000

    better error description and trying workaround

--- plug-ins/SextantePlugIn/src/es/unex/sextante/openjump/extensions/SextanteToolboxPlugin.java
+++ plug-ins/SextantePlugIn/src/es/unex/sextante/openjump/extensions/SextanteToolboxPlugin.java
@@ -34,22 +34,54 @@ public class SextanteToolboxPlugin
 
 
    public void initialize(final PlugInContext context) throws Exception {
-	  System.out.println("starting Sextante initialization ==");
-      Sextante.initialize(getJarsFolder()); // needs to be there for the distribution to load the algorithms,
+	  System.out.println("starting Sextante initialization >>");
+	  String jarspath = getJarsFolder();
+	  try{
+		  Sextante.initialize(jarspath); // needs to be there for the distribution to load the algorithms,
       										// but is not necessary for debugging newly created algorithms started from an OJ menu
-      Sextante.initialize(); //this will load only from classpath not from folder
-      SextanteGUI.setSextantePath(getHelpPath());
-      SextanteGUI.initialize();
-      SextanteGUI.setGUIFactory(new OpenJUMPGUIFactory());
-      SextanteGUI.setMainFrame(context.getWorkbenchFrame());
-      SextanteGUI.setInputFactory(new OpenJUMPInputFactory(context.getWorkbenchContext()));
-      SextanteGUI.setOutputFactory(new OpenJUMPOutputFactory(context.getWorkbenchContext()));
-      SextanteGUI.setPostProcessTaskFactory(new OpenJUMPPostProcessTaskFactory());
-      
-      OJSextanteApiInitialiser.isInitialized = true;
+	      Sextante.initialize(); //this will load only from classpath not from folder
+	      SextanteGUI.setSextantePath(getHelpPath());
+	      SextanteGUI.initialize();
+	      SextanteGUI.setGUIFactory(new OpenJUMPGUIFactory());
+	      SextanteGUI.setMainFrame(context.getWorkbenchFrame());
+	      SextanteGUI.setInputFactory(new OpenJUMPInputFactory(context.getWorkbenchContext()));
+	      SextanteGUI.setOutputFactory(new OpenJUMPOutputFactory(context.getWorkbenchContext()));
+	      SextanteGUI.setPostProcessTaskFactory(new OpenJUMPPostProcessTaskFactory());
+	      
+	      OJSextanteApiInitialiser.isInitialized = true;
+	
+	      context.getFeatureInstaller().addLayerViewMenuItem(this, new String[] { "Sextante" }, getName());
+	  }
+	  catch(Exception e){// this is most likely thrown while Debugging with the eclipse IDE.
+		System.out.println("Sextante not initialized!");
+		System.out.println("Check for problems with Sextante initialization, path for libs and resources: " + jarspath);
+		System.out.println("Check if image 'terminal.png' exist");
+		System.out.println("Check also for Sextante help classes path: " + getHelpPath());
+		System.out.println("=> Will do 2nd try with OJ-IDE-Dev settings: " );
+		// == do a second trial ==
+		String idePathSextante = System.getProperty("user.dir") + "/lib/plus/sextante";
+		String idePathSHelp = System.getProperty("user.dir") + "/lib/plus/sextante_help";
+		System.out.println("=> looking for algorithms and image in: " + idePathSextante);
+		try{
+			  Sextante.initialize(idePathSextante); 
+			  Sextante.initialize(); //this will load only from classpath not from folder
+			  SextanteGUI.setSextantePath(idePathSHelp);
+			  SextanteGUI.initialize();
+			  SextanteGUI.setGUIFactory(new OpenJUMPGUIFactory());
+			  SextanteGUI.setMainFrame(context.getWorkbenchFrame());
+			  SextanteGUI.setInputFactory(new OpenJUMPInputFactory(context.getWorkbenchContext()));
+			  SextanteGUI.setOutputFactory(new OpenJUMPOutputFactory(context.getWorkbenchContext()));
+			  SextanteGUI.setPostProcessTaskFactory(new OpenJUMPPostProcessTaskFactory());
 
-      context.getFeatureInstaller().addLayerViewMenuItem(this, new String[] { "Sextante" }, getName());
-
+			  OJSextanteApiInitialiser.isInitialized = true;
+			  System.out.println("Success initializing Sextante at 2nd trial!");
+			  context.getFeatureInstaller().addLayerViewMenuItem(this, new String[] { "Sextante" }, getName());
+		}
+		catch(Exception e1){// this is most likely thrown while Debugging with the eclipse IDE.
+			System.out.println("No success with Sextante initialization - printing error log:" );
+			e1.printStackTrace();
+		}
+	  }
    }
 
 
