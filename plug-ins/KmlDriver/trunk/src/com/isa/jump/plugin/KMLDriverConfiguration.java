d249ddc6a8d9cb78f07d0e19fd6f8deeb6c9ed2f
edso <edso@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2015-04-05 14:36:17 +0000

    move version variable to build.xml header  move changes into this dedicated file  create resources/kml/ProjectStringsList.pjl which is needed    UTM reprojection, add it to ant targets

--- plug-ins/KmlDriver/trunk/src/com/isa/jump/plugin/KMLDriverConfiguration.java
+++ plug-ins/KmlDriver/trunk/src/com/isa/jump/plugin/KMLDriverConfiguration.java
@@ -22,32 +22,29 @@
 
 package com.isa.jump.plugin;
 
+import com.vividsolutions.jump.I18N;
 import com.vividsolutions.jump.workbench.plugin.Extension;
 import com.vividsolutions.jump.workbench.plugin.PlugInContext;
-import org.openjump.core.ui.io.file.DataSourceFileLayerLoader;
-
-import java.net.URL;
-import java.net.URLClassLoader;
 
 /**
  * This is the entry class to declare the kml driver to JUMP.
  * Put the .jar file containing the driver in your installation ext directory.
- * @author Micha&euml;l Michaud
- * @version 0.2.3
  */
-// History
-// 0.2.4 (2015-04-02) reenable loading of kmz files, reformatting removing tabs
-// 0.2.3 (2015-03-22) fix a regression introduced in 4215 with a change in core
-//                    OpenJUMP (making xml based drivers charset aware)
-// 0.2.2 (2014-12-21) make kml parser charset aware
-// 0.2.1 (2014-12-20) fix encoding problem (cf #383)
-// 0.2   (2014) version included in OpenJUMP 1.7 and 1.8 PLUS
-// 0.1   (2011-09-17)   : first version taken from SkyJUMP source
+
 public class KMLDriverConfiguration extends Extension {
 
-    public void configure(PlugInContext context) throws Exception {
-        new KMLDataSourceQueryChooserInstallerPlugIn().initialize(context);
-    }
-    public String getName() {return "KML driver";}
-    public String getVersion() {return "0.2.4 (2015-04-02)";}
+  private static I18N _I18N = I18N.getInstance("com.isa.jump.plugin");
+
+  public void configure(PlugInContext context) throws Exception {
+    new KMLDataSourceQueryChooserInstallerPlugIn().initialize(context);
+  }
+
+  public String getName() {
+    return _I18N.getText("driver-name");
+  }
+
+  public String getVersion() {
+    return _I18N.getText("driver-version") + " ("
+        + _I18N.getText("driver-date") + ")";
+  }
 }
