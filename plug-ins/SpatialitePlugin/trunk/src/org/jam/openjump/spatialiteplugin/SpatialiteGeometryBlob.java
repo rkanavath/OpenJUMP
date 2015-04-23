7f49c1938d34e871bbfb68ef1522603db180e470
jratike80 <jratike80@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2015-04-23 07:14:40 +0000

    Add GPL license and copyright (C) Jorge Almaraz to source files.

--- plug-ins/SpatialitePlugin/trunk/src/org/jam/openjump/spatialiteplugin/SpatialiteGeometryBlob.java
+++ plug-ins/SpatialitePlugin/trunk/src/org/jam/openjump/spatialiteplugin/SpatialiteGeometryBlob.java
@@ -1,3 +1,32 @@
+/*
+ * The Unified Mapping Platform (JUMP) is an extensible, interactive GUI 
+ * for visualizing and manipulating spatial features with geometry and attributes.
+ *
+ * JUMP is Copyright (C) 2003 Vivid Solutions
+ *
+ * This program implements extensions to JUMP and is
+ * Copyright (C) 2010 Jorge Almaraz.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * For more information, contact:
+ *
+ * Jukka Rahkonen
+ * jukka.rahkonen@latuviitta.fi
+ * 
+ */
 package org.jam.openjump.spatialiteplugin;
 
 import org.apache.log4j.Logger;
