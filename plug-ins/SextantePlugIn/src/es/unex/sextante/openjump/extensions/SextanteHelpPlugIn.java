c10ad4e4f51329eaffb396324e4dedb445d31fc1
ma15569 <ma15569@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2016-11-08 15:28:51 +0000

    Improved Sextante help. Reorganized class paths and menu

---
+++ plug-ins/SextantePlugIn/src/es/unex/sextante/openjump/extensions/SextanteHelpPlugIn.java
@@ -0,0 +1,42 @@
+package es.unex.sextante.openjump.extensions;
+
+import javax.swing.ImageIcon;
+
+import org.openjump.core.ui.plugin.sextante.dialog.SextanteHelpDialog;
+
+import com.vividsolutions.jump.task.TaskMonitor;
+import com.vividsolutions.jump.workbench.plugin.PlugInContext;
+import com.vividsolutions.jump.workbench.plugin.ThreadedPlugIn;
+import com.vividsolutions.jump.workbench.ui.images.IconLoader;
+
+import es.unex.sextante.openjump.language.I18NPlug;
+
+public class SextanteHelpPlugIn implements ThreadedPlugIn {
+    public static final ImageIcon ICON = IconLoader
+            .icon("information_16x16.png");
+
+    public void run(TaskMonitor monitor, PlugInContext context)
+            throws Exception {
+    }
+
+    public void initialize(PlugInContext context) throws Exception {
+        context.getFeatureInstaller().addMainMenuPlugin(this,
+                new String[] { "Sextante" }, getName(), false, ICON, null);
+    }
+
+    public String getName() {// Giuseppe Aruta - PlugIn Internationalized
+        // 2013_05_25//
+
+        return I18NPlug
+                .getI18N("es.unex.sextante.kosmo.extensions.SextanteHelpPlugin.help");
+
+    }
+
+    public boolean execute(PlugInContext context) throws Exception {
+
+        SextanteHelpDialog.createAndShowGUI(context);
+
+        return true;
+    }
+
+}
