2864b064221463f3720a2c6ea5c20aeaa03842f0
edso <edso@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2017-03-18 15:47:07 +0000

    readd license refine static I18N usage

--- plug-ins/CADExtension/trunk/src/org/openjump/advancedtools/language/I18NPlug.java
+++ plug-ins/CADExtension/trunk/src/org/openjump/advancedtools/language/I18NPlug.java
@@ -1,7 +1,26 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * For more information, contact:
+ *
+ * jump-pilot-devel@lists.sourceforge.net
+ * http://sourceforge.net/projects/jump-pilot
+ */
 package org.openjump.advancedtools.language;
 
 import java.io.File;
-
 import com.vividsolutions.jump.I18N;
 
 /**
@@ -10,14 +29,13 @@ import com.vividsolutions.jump.I18N;
  *
  */
 public class I18NPlug {
-    private static I18N i18n = I18N.getInstance(new File("org/openjump/advancedtools/language/cadtoolbox"));
+    private static File path = new File("org/openjump/advancedtools/language/cadtoolbox");
 
     public static String getI18N(String key) {
-        return i18n.getText(key);
+        return getMessage(key);
     }
 
-    public static String getMessage(String label, Object[] objects) {
-        return i18n.getMessage(label, objects);
+    public static String getMessage(String label, final Object... objects) {
+        return I18N.getMessage(path, label, objects);
     }
-
 }
