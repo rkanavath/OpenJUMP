7f33360b66a42d6985eb795b861b4fda11c064c9
ma15569 <ma15569@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2017-03-21 15:47:52 +0000

    minor fix and added German file language

--- plug-ins/CADExtension/trunk/src/org/openjump/advancedtools/utils/CoordinateListMetricsUtils.java
+++ plug-ins/CADExtension/trunk/src/org/openjump/advancedtools/utils/CoordinateListMetricsUtils.java
@@ -1,7 +1,5 @@
 package org.openjump.advancedtools.utils;
 
-import java.text.DecimalFormat;
-
 import org.openjump.advancedtools.language.I18NPlug;
 
 import com.vividsolutions.jts.geom.Coordinate;
@@ -30,44 +28,46 @@ public class CoordinateListMetricsUtils {
     public static final String Measure = I18NPlug
             .getI18N("org.openjump.core.ui.utils.CoordinateListMetrics.Measure");
 
-    public static DecimalFormat df2 = new DecimalFormat("##0.0##");
+    // public static DecimalFormat df2 = new DecimalFormat("##0.0##");
+
+    public static double format(double d) {
+        return new Double(new Long(Math.round(d * 100)).doubleValue() / 100.0D);
+
+    }
 
     public static String circleString(double a, double b, Coordinate start,
             Coordinate target) {
-        String all = Coordinates + " [" + Center + start.x + "," + start.y
-                + " - " + Target + target.x + "," + target.y + "]  " + Radius
-                + df2.format(a) + "  " + Circum + df2.format(b);
+        String all = Coordinates + " [" + Center + start.x + ";" + start.y
+                + " - " + Target + target.x + ";" + target.y + "]  " + Radius
+                + format(a) + "  " + Circum + format(b);
         return all;
 
     }
 
     public static void setCircleMessage(double a, double b, Coordinate start,
             Coordinate target) {
-        String all = Coordinates + " [" + Center + start.x + "," + start.y
-                + " - " + Target + target.x + "," + target.y + "]  " + Radius
-                + df2.format(a) + "  " + Circum + df2.format(b);
+        String all = Coordinates + " [" + Center + start.x + ";" + start.y
+                + " - " + Target + target.x + ";" + target.y + "]  " + Radius
+                + format(a) + "  " + Circum + format(b);
         JUMPWorkbench.getInstance().getFrame().getContext().getLayerViewPanel()
                 .getContext().setStatusMessage(all);
     }
 
     public static void setCircleHTMLFrame(double a, double b, Coordinate start,
             Coordinate target) {
-        String all = Coordinates + " [" + Center + start.x + "," + start.y
-                + " - " + Target + target.x + "," + target.y + "]  " + Radius
-                + df2.format(a) + "  " + Circum + df2.format(b);
         HTMLFrame out = JUMPWorkbench.getInstance().getContext()
                 .createPlugInContext().getOutputFrame();
         out.createNewDocument();
         out.addHeader(1, Measure);
         out.addHeader(2, Circle);
-        out.addHeader(3, Coordinates + " [" + Center + start.x + "," + start.y
-                + " - " + Target + target.x + "," + target.y + "]  ");
-        out.addHeader(3, Radius + df2.format(a));
-        out.addHeader(3, Circum + df2.format(b));
+        out.addHeader(3, Coordinates + " [" + Center + start.x + ";" + start.y
+                + " - " + Target + target.x + ";" + target.y + "]  ");
+        out.addHeader(3, Radius + format(a));
+        out.addHeader(3, Circum + format(b));
     }
 
     public static void setMessage(String aString, double a) {
-        String all = aString + df2.format(a);
+        String all = aString + format(a);
         JUMPWorkbench.getInstance().getFrame().getContext().getLayerViewPanel()
                 .getContext().setStatusMessage(all);
     }
