daa2116241642c4b854c49a829ed74330437290e
ma15569 <ma15569@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2018-01-28 07:22:47 +0000

    Remove depency to OpenJUMP AdditionalResult.class as it requires new other tests before to realize. Sextante help opens as OJ internal frame. Added new build: ojsextante_binding_gen2018.jar

--- plug-ins/SextantePlugIn/src/es/unex/sextante/openjump/extensions/SextanteResultsPlugin.java
+++ plug-ins/SextantePlugIn/src/es/unex/sextante/openjump/extensions/SextanteResultsPlugin.java
@@ -4,19 +4,18 @@ import java.util.ArrayList;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
-import javax.swing.JInternalFrame;
 import javax.swing.JOptionPane;
 
 import org.openjump.core.ui.plugin.AbstractUiPlugIn;
-import org.openjump.sextante.core.ObjectAndDescription;
-import org.openjump.sextante.gui.additionalResults.AdditionalResults;
-import org.openjump.sextante.gui.additionalResults.AdditionalResultsFrame;
 
-import com.vividsolutions.jump.I18N;
-import com.vividsolutions.jump.workbench.plugin.PlugInContext;
-
+import es.unex.sextante.core.Sextante;
+import es.unex.sextante.gui.additionalResults.AdditionalResults;
+import es.unex.sextante.gui.core.SextanteGUI;
 import es.unex.sextante.openjump.language.I18NPlug;
 //import es.unex.sextante.gui.additionalResults.AdditionalResults;
+//import org.openjump.sextante.gui.additionalResults.AdditionalResultsFrame;
+import com.vividsolutions.jump.workbench.plugin.PlugInContext;
+import com.vividsolutions.jump.I18N;
 
 //import es.unex.sextante.gui.additionalResults.AdditionalResults;
 
@@ -32,38 +31,36 @@ public class SextanteResultsPlugin extends AbstractUiPlugIn {
     @Override
     public boolean execute(final PlugInContext context) throws Exception {
 
-        ArrayList<ObjectAndDescription> m_Components = AdditionalResults.m_Components;
-        if (m_Components == null || m_Components.size() == 0) {
-            JOptionPane.showMessageDialog(null, sWarning, sName,
-                    JOptionPane.WARNING_MESSAGE);
-            return false;
-        } else {
+        /*
+         * ArrayList<ObjectAndDescription> m_Components =
+         * AdditionalResults.m_Components; if (m_Components == null ||
+         * m_Components.size() == 0) { JOptionPane.showMessageDialog(null,
+         * sWarning, sName, JOptionPane.WARNING_MESSAGE); return false; } else {
+         * 
+         * for (JInternalFrame iFrame : context.getWorkbenchFrame()
+         * .getInternalFrames()) { if (iFrame instanceof AdditionalResultsFrame)
+         * {
+         * 
+         * iFrame.toFront(); return true;
+         * 
+         * } } AdditionalResultsFrame additionalResultsFrame = new
+         * AdditionalResultsFrame( m_Components);
+         * 
+         * context.getWorkbenchFrame()
+         * .addInternalFrame(additionalResultsFrame);
+         * 
+         * }
+         */
 
-            for (JInternalFrame iFrame : context.getWorkbenchFrame()
-                    .getInternalFrames()) {
-                if (iFrame instanceof AdditionalResultsFrame) {
-
-                    iFrame.toFront();
-                    return true;
-
-                }
-            }
-            AdditionalResultsFrame additionalResultsFrame = new AdditionalResultsFrame(
-                    m_Components);
-
-            context.getWorkbenchFrame()
-                    .addInternalFrame(additionalResultsFrame);
-
+        final ArrayList<?> results = AdditionalResults.getComponents();
+        if (results.size() != 0) {
+            SextanteGUI.getGUIFactory().showAdditionalResultsDialog(results);
         }
 
-        /*
-         * final ArrayList<?> results = AdditionalResults.getComponents(); if
-         * (results.size() != 0) {
-         * SextanteGUI.getGUIFactory().showAdditionalResultsDialog(results); }
-         * 
-         * else { JOptionPane.showMessageDialog(null, NO_RESULTS,
-         * Sextante.getText("Warning"), JOptionPane.WARNING_MESSAGE); }
-         */
+        else {
+            JOptionPane.showMessageDialog(null, NO_RESULTS,
+                    Sextante.getText("Warning"), JOptionPane.WARNING_MESSAGE);
+        }
 
         return true;
 
--- plug-ins/SextantePlugIn/src/es/unex/sextante/openjump/gui/OpenJUMPPostProcessTask.java
+++ plug-ins/SextantePlugIn/src/es/unex/sextante/openjump/gui/OpenJUMPPostProcessTask.java
@@ -9,20 +9,21 @@ import javax.swing.JTextPane;
 import javax.swing.border.BevelBorder;
 
 import org.openjump.core.rasterimage.RasterImageLayer;
+//import org.openjump.sextante.core.ObjectAndDescription;
 
 import com.vividsolutions.jump.workbench.WorkbenchContext;
 import com.vividsolutions.jump.workbench.model.Layer;
 import com.vividsolutions.jump.workbench.model.StandardCategoryNames;
 
 import es.unex.sextante.core.GeoAlgorithm;
-//import es.unex.sextante.core.ObjectAndDescription;
-import org.openjump.sextante.core.ObjectAndDescription;
+import es.unex.sextante.core.ObjectAndDescription;
 import es.unex.sextante.core.OutputObjectsSet;
 import es.unex.sextante.core.Sextante;
 import es.unex.sextante.dataObjects.IRasterLayer;
 import es.unex.sextante.dataObjects.ITable;
 import es.unex.sextante.dataObjects.IVectorLayer;
 import es.unex.sextante.gui.additionalResults.AdditionalResults;
+//import es.unex.sextante.gui.additionalResults.AdditionalResults;
 import es.unex.sextante.gui.additionalResults.TableTools;
 import es.unex.sextante.gui.core.SextanteGUI;
 import es.unex.sextante.gui.settings.SextanteGeneralSettings;
@@ -37,142 +38,163 @@ import es.unex.sextante.outputs.OutputTable;
 import es.unex.sextante.outputs.OutputText;
 import es.unex.sextante.outputs.OutputVectorLayer;
 
-public class OpenJUMPPostProcessTask
-         implements
-            Runnable {
+public class OpenJUMPPostProcessTask implements Runnable {
 
-   private final OutputObjectsSet m_Output;
-   private final boolean          m_bShowResultsDialog;
+    private final OutputObjectsSet m_Output;
+    private final boolean m_bShowResultsDialog;
 
+    public OpenJUMPPostProcessTask(final GeoAlgorithm algorithm,
+            final boolean showResultsDialog) {
 
-   public OpenJUMPPostProcessTask(final GeoAlgorithm algorithm,
-                                  final boolean showResultsDialog) {
+        m_Output = algorithm.getOutputObjects();
+        m_bShowResultsDialog = showResultsDialog;
 
-      m_Output = algorithm.getOutputObjects();
-      m_bShowResultsDialog = showResultsDialog;
+    }
 
-   }
+    @Override
+    public void run() {
 
+        addResults();
 
-   public void run() {
+    }
 
-      addResults();
+    private boolean addResults() {
 
-   }
+        String sDescription;
+        boolean bShowAdditionalPanel = false;
+        final boolean bUseInternalNames = new Boolean(
+                SextanteGUI
+                        .getSettingParameterValue(SextanteGeneralSettings.USE_INTERNAL_NAMES))
+                .booleanValue();
+        final boolean bModiFyResultsNames = new Boolean(
+                SextanteGUI
+                        .getSettingParameterValue(SextanteGeneralSettings.MODIFY_NAMES))
+                .booleanValue();
 
+        for (int i = 0; i < m_Output.getOutputObjectsCount(); i++) {
 
-   private boolean addResults() {
-
-      String sDescription;
-      boolean bShowAdditionalPanel = false;
-      final boolean bUseInternalNames = new Boolean(
-               SextanteGUI.getSettingParameterValue(SextanteGeneralSettings.USE_INTERNAL_NAMES)).booleanValue();
-      final boolean bModiFyResultsNames = new Boolean(SextanteGUI.getSettingParameterValue(SextanteGeneralSettings.MODIFY_NAMES)).booleanValue();
-
-      for (int i = 0; i < m_Output.getOutputObjectsCount(); i++) {
-
-         final Output out = m_Output.getOutput(i);
-         sDescription = out.getDescription();
-         final IOutputChannel channel = out.getOutputChannel();
-         final Object object = out.getOutputObject();
-         if ((out instanceof OutputRasterLayer) || (out instanceof Output3DRasterLayer) || (out instanceof OutputTable)
-             || (out instanceof OutputVectorLayer)) {
-            if (bUseInternalNames) {
-               sDescription = out.getName();
+            final Output out = m_Output.getOutput(i);
+            sDescription = out.getDescription();
+            final IOutputChannel channel = out.getOutputChannel();
+            final Object object = out.getOutputObject();
+            if ((out instanceof OutputRasterLayer)
+                    || (out instanceof Output3DRasterLayer)
+                    || (out instanceof OutputTable)
+                    || (out instanceof OutputVectorLayer)) {
+                if (bUseInternalNames) {
+                    sDescription = out.getName();
+                } else if (bModiFyResultsNames) {
+                    sDescription = SextanteGUI.modifyResultName(sDescription);
+                }
+                if ((channel instanceof NullOutputChannel) || (channel == null)) {
+                    continue;
+                }
             }
-            else if (bModiFyResultsNames) {
-               sDescription = SextanteGUI.modifyResultName(sDescription);
-            }
-            if ((channel instanceof NullOutputChannel) || (channel == null)) {
-               continue;
-            }
-         }
-         if (object instanceof IRasterLayer) {
-            final OpenJUMPOutputFactory factory = (OpenJUMPOutputFactory) SextanteGUI.getOutputFactory();
-            final WorkbenchContext context = factory.getContext();
-            final IRasterLayer layer = (IRasterLayer) object;
-            RasterImageLayer openJUMPLayer = (RasterImageLayer) layer.getBaseDataObject();
-            if (openJUMPLayer == null) {
-               openJUMPLayer = (RasterImageLayer) ((IRasterLayer) SextanteGUI.getInputFactory().openDataObjectFromFile(
-                        ((FileOutputChannel) channel).getFilename())).getBaseDataObject();
-            }
-            context.getLayerManager().addLayerable(StandardCategoryNames.WORKING, openJUMPLayer);
-            SextanteGUI.getInputFactory().addDataObject(layer);
-         }
-         else if (object instanceof IVectorLayer) {
-            final OpenJUMPOutputFactory factory = (OpenJUMPOutputFactory) SextanteGUI.getOutputFactory();
-            final WorkbenchContext context = factory.getContext();
-            final IVectorLayer layer = (IVectorLayer) object;
-            Layer openJUMPLayer = (Layer) layer.getBaseDataObject();
-            if (openJUMPLayer == null) {
-               openJUMPLayer = (Layer) ((IVectorLayer) SextanteGUI.getInputFactory().openDataObjectFromFile(
-                        ((FileOutputChannel) channel).getFilename())).getBaseDataObject();
-            }
-            context.getLayerManager().addLayer(StandardCategoryNames.WORKING, openJUMPLayer);
-            SextanteGUI.getInputFactory().addDataObject(layer);
-         }
-         else if (out instanceof OutputTable) {
-            try {
-               final JScrollPane jScrollPane = TableTools.getScrollableTablePanelFromITable((ITable) object);
-            // [Giuseppe Aruta 2017-12-11] moved output to OpenJUMP
-               // Internal Frame
-               // AdditionalResults.addComponent(new ObjectAndDescription(
-               // sDescription, jScrollPane));
-               // bShowAdditionalPanel = true;
+            if (object instanceof IRasterLayer) {
+                final OpenJUMPOutputFactory factory = (OpenJUMPOutputFactory) SextanteGUI
+                        .getOutputFactory();
+                final WorkbenchContext context = factory.getContext();
+                final IRasterLayer layer = (IRasterLayer) object;
+                RasterImageLayer openJUMPLayer = (RasterImageLayer) layer
+                        .getBaseDataObject();
+                if (openJUMPLayer == null) {
+                    openJUMPLayer = (RasterImageLayer) ((IRasterLayer) SextanteGUI
+                            .getInputFactory()
+                            .openDataObjectFromFile(
+                                    ((FileOutputChannel) channel).getFilename()))
+                            .getBaseDataObject();
+                }
+                context.getLayerManager().addLayerable(
+                        StandardCategoryNames.WORKING, openJUMPLayer);
+                SextanteGUI.getInputFactory().addDataObject(layer);
+            } else if (object instanceof IVectorLayer) {
+                final OpenJUMPOutputFactory factory = (OpenJUMPOutputFactory) SextanteGUI
+                        .getOutputFactory();
+                final WorkbenchContext context = factory.getContext();
+                final IVectorLayer layer = (IVectorLayer) object;
+                Layer openJUMPLayer = (Layer) layer.getBaseDataObject();
+                if (openJUMPLayer == null) {
+                    openJUMPLayer = (Layer) ((IVectorLayer) SextanteGUI
+                            .getInputFactory()
+                            .openDataObjectFromFile(
+                                    ((FileOutputChannel) channel).getFilename()))
+                            .getBaseDataObject();
+                }
+                context.getLayerManager().addLayer(
+                        StandardCategoryNames.WORKING, openJUMPLayer);
+                SextanteGUI.getInputFactory().addDataObject(layer);
+            } else if (out instanceof OutputTable) {
+                try {
+                    final JScrollPane jScrollPane = TableTools
+                            .getScrollableTablePanelFromITable((ITable) object);
+                    // [Giuseppe Aruta 2017-12-11] moved output to OpenJUMP
+                    // Internal Frame
+                    AdditionalResults.addComponent(new ObjectAndDescription(
+                            sDescription, jScrollPane));
+                    bShowAdditionalPanel = true;
 
-               org.openjump.sextante.gui.additionalResults.AdditionalResults
-                       .addComponentAndShow(new ObjectAndDescription(
-                               sDescription, jScrollPane));
+                    // org.openjump.sextante.gui.additionalResults.AdditionalResults
+                    // .addComponentAndShow(new ObjectAndDescription(
+                    // sDescription, jScrollPane));
+
+                } catch (final Exception e) {
+                    Sextante.addErrorToLog(e);
+                }
+            } else if (out instanceof OutputText) {
+                JTextPane jTextPane;
+                JScrollPane jScrollPane;
+                jTextPane = new JTextPane();
+                jTextPane.setEditable(false);
+                jTextPane.setContentType("text/html");
+                jTextPane.setText((String) object);
+                jScrollPane = new JScrollPane();
+                jScrollPane.setViewportView(jTextPane);
+                jScrollPane
+                        .setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+                jTextPane.setBorder(BorderFactory
+                        .createEtchedBorder(BevelBorder.LOWERED));
+                // [Giuseppe Aruta 2017-12-11] moved output to OpenJUMP Internal
+                // Frame
+                // org.openjump.sextante.gui.additionalResults.AdditionalResults
+                // .addComponentAndShow(new ObjectAndDescription(
+                // sDescription, jScrollPane));
+                AdditionalResults.addComponent(new ObjectAndDescription(
+                        sDescription, jScrollPane));
+                // org.openjump.core.ui.plugin.additionalResults.AdditionalResults
+                // .addComponent(new ObjectAndDescription(sDescription,
+                // jScrollPane));
+                bShowAdditionalPanel = true;
+            } else if (object instanceof Component) {
+                // [Giuseppe Aruta 2017-12-11] moved output to OpenJUMP Internal
+                // Frame
+                // org.openjump.sextante.gui.additionalResults.AdditionalResults
+                // .addComponentAndShow(new ObjectAndDescription(
+                // sDescription, object));
+                AdditionalResults.addComponent(new ObjectAndDescription(
+                        sDescription, object));
+                // org.openjump.core.ui.plugin.additionalResults.AdditionalResults
+                // .addComponent(new ObjectAndDescription(sDescription,
+                // object));
+                bShowAdditionalPanel = true;
+            } else if (out instanceof Output3DRasterLayer) {
+                JOptionPane.showMessageDialog(SextanteGUI.getMainFrame(),
+                        Sextante.getText("3d_not_supported"),
+                        Sextante.getText("Warning"),
+                        JOptionPane.WARNING_MESSAGE);
             }
-            catch (final Exception e) {
-               Sextante.addErrorToLog(e);
-            }
-         }
-         else if (out instanceof OutputText) {
-            JTextPane jTextPane;
-            JScrollPane jScrollPane;
-            jTextPane = new JTextPane();
-            jTextPane.setEditable(false);
-            jTextPane.setContentType("text/html");
-            jTextPane.setText((String) object);
-            jScrollPane = new JScrollPane();
-            jScrollPane.setViewportView(jTextPane);
-            jScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-            jTextPane.setBorder(BorderFactory.createEtchedBorder(BevelBorder.LOWERED));
+
+        }
+
+        if (bShowAdditionalPanel && m_bShowResultsDialog) {
             // [Giuseppe Aruta 2017-12-11] moved output to OpenJUMP Internal
             // Frame
-            org.openjump.sextante.gui.additionalResults.AdditionalResults
-            .addComponentAndShow(new ObjectAndDescription(
-                    sDescription, jScrollPane));
-         //   AdditionalResults.addComponent(new ObjectAndDescription(sDescription, jScrollPane));
-         //   bShowAdditionalPanel = true;
-         }
-         else if (object instanceof Component) {
-          // [Giuseppe Aruta 2017-12-11] moved output to OpenJUMP Internal
-             // Frame
-             org.openjump.sextante.gui.additionalResults.AdditionalResults
-             .addComponentAndShow(new ObjectAndDescription(
-                     sDescription, object));
-         //   AdditionalResults.addComponent(new ObjectAndDescription(sDescription, object));
-         //   bShowAdditionalPanel = true;
-         }
-         else if (out instanceof Output3DRasterLayer) {
-            JOptionPane.showMessageDialog(SextanteGUI.getMainFrame(), Sextante.getText("3d_not_supported"),
-                     Sextante.getText("Warning"), JOptionPane.WARNING_MESSAGE);
-         }
+            // org.openjump.sextante.gui.additionalResults.AdditionalResults
+            // .showPanel();
+            AdditionalResults.showPanel();
+        }
 
-      }
+        return true;
 
-      if (bShowAdditionalPanel && m_bShowResultsDialog) {
-          // [Giuseppe Aruta 2017-12-11] moved output to OpenJUMP Internal
-          // Frame
-          org.openjump.sextante.gui.additionalResults.AdditionalResults
-                  .showPanel();
-      //   AdditionalResults.showPanel();
-      }
-
-      return true;
-
-   }
+    }
 
 }
