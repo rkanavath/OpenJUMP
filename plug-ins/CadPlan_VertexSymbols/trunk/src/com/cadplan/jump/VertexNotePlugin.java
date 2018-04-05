81817029541b20f2fdecfabb9381f7aef6057b31
ma15569 <ma15569@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2018-04-05 17:24:10 +0000

    Updated menus and language files

--- plug-ins/CadPlan_VertexSymbols/trunk/src/com/cadplan/jump/VertexNotePlugin.java
+++ plug-ins/CadPlan_VertexSymbols/trunk/src/com/cadplan/jump/VertexNotePlugin.java
@@ -1,5 +1,8 @@
 package com.cadplan.jump;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
 import com.vividsolutions.jump.workbench.plugin.AbstractPlugIn;
 import com.vividsolutions.jump.workbench.plugin.EnableCheck;
 import com.vividsolutions.jump.workbench.plugin.EnableCheckFactory;
@@ -7,67 +10,81 @@ import com.vividsolutions.jump.workbench.plugin.MultiEnableCheck;
 import com.vividsolutions.jump.workbench.plugin.PlugInContext;
 import com.vividsolutions.jump.workbench.ui.LayerViewPanel;
 import com.vividsolutions.jump.workbench.ui.MenuNames;
-import com.vividsolutions.jump.workbench.ui.WorkbenchToolBar;
-import com.cadplan.fileio.IconLoader;
-
-import javax.swing.*;
-
-import java.awt.*;
 
 /**
- * User: geoff
- * Date: 1/08/2007
- * Time: 08:24:50
- * Copyright 2005 Geoffrey G Roy.
+ * User: geoff Date: 1/08/2007 Time: 08:24:50 Copyright 2005 Geoffrey G Roy.
  */
-public class VertexNotePlugin extends AbstractPlugIn
-{
+public class VertexNotePlugin extends AbstractPlugIn {
 
     private I18NPlug iPlug;
 
-    public void initialize(PlugInContext context) throws Exception
-    {
-        iPlug = new I18NPlug("VertexSymbols","language.VertexSymbolsPlugin");
-        
-        EnableCheckFactory check = new EnableCheckFactory(context.getWorkbenchContext());
-        EnableCheck scheck = check.createAtLeastNFeaturesMustBeSelectedCheck(1);
-        MultiEnableCheck mcheck = new MultiEnableCheck();
-        
+    @Override
+    public void initialize(PlugInContext context) throws Exception {
+        iPlug = new I18NPlug("VertexSymbols", "language.VertexSymbolsPlugin");
+
+        final EnableCheckFactory check = new EnableCheckFactory(
+                context.getWorkbenchContext());
+        final EnableCheck scheck = check
+                .createAtLeastNFeaturesMustBeSelectedCheck(1);
+        final MultiEnableCheck mcheck = new MultiEnableCheck();
+
         mcheck.add(check.createAtLeastNLayersMustExistCheck(1));
-        mcheck.add(check.createAtLeastNLayersMustBeEditableCheck(1)); 
-        //mcheck.add(check.createAtLeastNFeaturesMustBeSelectedCheck(1));
-        
-        String menuName = MenuNames.PLUGINS; //iPlug.get("VertexSymbols.MenuName");
-        String menuItem = "Vertex Note"; //iPlug.get("VertexSymbols.MenuItem");
-        context.getFeatureInstaller().addMainMenuItem(this, new String[] {menuName},
-                     menuItem, false, null,scheck );
-        
-        
+        mcheck.add(check.createAtLeastNLayersMustBeEditableCheck(1));
+        // mcheck.add(check.createAtLeastNFeaturesMustBeSelectedCheck(1));
 
-        String dirName = context.getWorkbenchContext().getWorkbench().getPlugInManager().getPlugInDirectory().getAbsolutePath();
-        IconLoader loader = new IconLoader(dirName,"VertexSymbols");
+        final String menuName = MenuNames.PLUGINS; // iPlug.get("VertexSymbols.MenuName");
+        context.getFeatureInstaller().addMainMenuPlugin(this,
+                new String[] { menuName }, getName(), false, getIcon(), mcheck);
+        context.getWorkbenchFrame()
+                .getToolBar()
+                .addPlugIn(getIcon(), this, mcheck,
+                        context.getWorkbenchContext());
+        context.getFeatureInstaller().addPopupMenuPlugin(
+                LayerViewPanel.popupMenu(), this, getName(), false, getIcon(),
+                scheck);
 
-        //Image image = loader.loadImage("noteicon.gif");
-        //ImageIcon icon = new ImageIcon(image);
-        //System.out.println("Note Resource path: "+this.getClass().getResource("/Resources/noteicon.gif"));
-        ImageIcon icon = new ImageIcon(this.getClass().getResource("/Resources/noteicon.gif"));
+        // final String menuItem = "Vertex Note"; //
+        // iPlug.get("VertexSymbols.MenuItem");
+        // context.getFeatureInstaller().addMainMenuItem(this,
+        // new String[] { menuName }, menuItem, false, null, scheck);
 
-        
-        
-        context.getFeatureInstaller().addPopupMenuItem(LayerViewPanel.popupMenu(),this, menuItem, false, icon, 
-        		scheck );
-        
-        WorkbenchToolBar toolBar = context.getWorkbenchFrame().getToolBar();
-        
-        
-        JButton button = toolBar.addPlugIn(icon,this,mcheck ,context.getWorkbenchContext());
-        //JButton button = toolBar.addPlugIn(new ImageIcon(image),this,check.createAtLeastNFeaturesMustBeSelectedCheck(1),context.getWorkbenchContext());
+        // final String dirName = context.getWorkbenchContext().getWorkbench()
+        // .getPlugInManager().getPlugInDirectory().getAbsolutePath();
+        // final IconLoader loader = new IconLoader(dirName, "VertexSymbols");
+
+        // Image image = loader.loadImage("noteicon.gif");
+        // ImageIcon icon = new ImageIcon(image);
+        // System.out.println("Note Resource path: "+this.getClass().getResource("/Resources/noteicon.gif"));
+        // final ImageIcon icon = new ImageIcon(this.getClass().getResource(
+        // "/Resources/noteicon.gif"));
+
+        // context.getFeatureInstaller()
+        // .addPopupMenuItem(LayerViewPanel.popupMenu(), this,menuItem,
+        // false, icon, scheck);
+
+        // final WorkbenchToolBar toolBar = context.getWorkbenchFrame()
+        // .getToolBar();
+
+        // final JButton button = toolBar.addPlugIn(icon, this, mcheck,
+        // context.getWorkbenchContext());
+        // JButton button = toolBar.addPlugIn(new
+        // ImageIcon(image),this,check.createAtLeastNFeaturesMustBeSelectedCheck(1),context.getWorkbenchContext());
 
     }
 
-    public boolean execute(PlugInContext context) throws Exception
-    {
-        VertexNote vn = new VertexNote(context,iPlug);
+    public Icon getIcon() {
+        return new ImageIcon(getClass().getResource("/Resources/noteicon.gif"));
+    }
+
+    @Override
+    public String getName() {
+        iPlug = new I18NPlug("VertexSymbols", "language.VertexSymbolsPlugin");
+        return iPlug.get("VertexNote.MenuItem");
+    }
+
+    @Override
+    public boolean execute(PlugInContext context) throws Exception {
+        final VertexNote vn = new VertexNote(context, iPlug);
         return true;
     }
 }
