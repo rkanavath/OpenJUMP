752b167db9ef59b8f7c29a5de12bb8c1ea024aae
ma15569 <ma15569@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2018-10-10 09:03:45 +0000

    Added real area algorithm

---
+++ plug-ins/OpenKLEM/OpenKLEMCore/trunk/src/com/geomaticaeambiente/openjump/klem/realArea/RealAreaCalculator.java
@@ -0,0 +1,95 @@
+package com.geomaticaeambiente.openjump.klem.realArea;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import com.geomaticaeambiente.openjump.klem.grid.DoubleBasicGrid;
+import com.geomaticaeambiente.openjump.klem.grid.GridDestriper;
+import com.geomaticaeambiente.openjump.klem.grid.GridStriper;
+import com.geomaticaeambiente.openjump.klem.parallel.ExecutorBuilder;
+
+/**
+ *
+ * @author AdL
+ */
+public class RealAreaCalculator {
+
+    /**
+     * Calculates the hillshade
+     * 
+     * @param slopeDegsGrid
+     * @param aspectDegsGrid
+     * @param zenithDegs
+     * @param azimuthDegs
+     */
+    public RealAreaCalculator(DoubleBasicGrid slopeDegsGrid, double zenithDegs,
+            double azimuthDegs) {
+        this.slopeDegsGrid = slopeDegsGrid;
+
+        this.zenithDegs = zenithDegs;
+        this.azimuthDegs = azimuthDegs;
+    }
+
+    /**
+     * Calculates the aspect (in radians).
+     * 
+     * @return The aspect (radians).
+     * @throws InterruptedException
+     * @throws ExecutionException
+     * @throws IOException
+     */
+    public DoubleBasicGrid calculate() throws InterruptedException,
+            ExecutionException, IOException {
+
+        final int rowCount = slopeDegsGrid.getRowCount();
+        final ExecutorBuilder execBuilder = new ExecutorBuilder(rowCount);
+        final int stripeRowCount = execBuilder.getRowPerStripeCount();
+        final int stripeCount = execBuilder.getStripeCount();
+
+        final List<Callable<DoubleBasicGrid>> asepectToDos_l = new ArrayList<Callable<DoubleBasicGrid>>();
+
+        final DoubleBasicGrid[] hillshadeDegStripes = GridStriper
+                .stripeDoubleGrid(slopeDegsGrid, stripeRowCount);
+
+        /* Stripe */
+        for (int s = 0; s < stripeCount; s++) {
+
+            int stripeEffectiveRows;
+            if (s != stripeCount - 1) {
+                stripeEffectiveRows = stripeRowCount;
+            } else {
+                stripeEffectiveRows = rowCount - (s * stripeRowCount);
+            }
+
+            final int offset = s * stripeRowCount;
+
+            final RealAreaStripe aspectStripe = new RealAreaStripe(
+                    stripeEffectiveRows, slopeDegsGrid, offset, zenithDegs,
+                    azimuthDegs);
+
+            asepectToDos_l.add(aspectStripe);
+
+        }
+
+        /* Calculate */
+        final List<Future<DoubleBasicGrid>> aspectFutures_l = execBuilder
+                .getExecutorService().invokeAll(asepectToDos_l);
+        final List<DoubleBasicGrid> aspectGrids_l = new ArrayList<DoubleBasicGrid>();
+        for (final Future<DoubleBasicGrid> asepctFuture : aspectFutures_l) {
+            aspectGrids_l.add(asepctFuture.get());
+        }
+
+        return GridDestriper.assembleDoubleStrips(aspectGrids_l);
+
+    }
+
+    private final DoubleBasicGrid slopeDegsGrid;
+
+    private final double zenithDegs;
+    private final double azimuthDegs;
+
+}
---
+++ plug-ins/OpenKLEM/OpenKLEMCore/trunk/src/com/geomaticaeambiente/openjump/klem/realArea/RealAreaStripe.java
@@ -0,0 +1,78 @@
+package com.geomaticaeambiente.openjump.klem.realArea;
+
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+import com.geomaticaeambiente.openjump.klem.grid.DoubleBasicGrid;
+
+/**
+ *
+ * @author AdL
+ */
+public class RealAreaStripe implements Callable<DoubleBasicGrid> {
+
+    public RealAreaStripe(int stripeEffectiveRows,
+            DoubleBasicGrid slopeDegsGrid, int yOffset, double zenithDegs,
+            double azimuthDegs) {
+
+        this.stripeEffectiveRows = stripeEffectiveRows;
+        this.slopeDegsGrid = slopeDegsGrid;
+
+        this.yOffset = yOffset;
+
+    }
+
+    @Override
+    public DoubleBasicGrid call() throws Exception {
+        return calcAspect();
+    }
+
+    private DoubleBasicGrid calcAspect() throws IOException {
+
+        final int nRows = stripeEffectiveRows;
+        final int nCols = slopeDegsGrid.getColumnCount();
+
+        final DoubleBasicGrid realAreaGrid = new DoubleBasicGrid(
+                new byte[nRows][nCols], slopeDegsGrid.getCellSize(), -9999,
+                slopeDegsGrid.getLowerLeftCoord());
+
+        for (int row = 0; row < nRows; row++) {
+            for (int col = 0; col < nCols; col++) {
+
+                realAreaGrid.setValue(col, row, slopeDegsGrid.getNoData());
+
+                final java.awt.Point cell = new java.awt.Point(col, row
+                        + yOffset);
+
+                if (!slopeDegsGrid.isNoData(slopeDegsGrid.getValue(cell))) {
+
+                    final double area = slopeDegsGrid.getCellSize()
+                            * slopeDegsGrid.getCellSize();
+                    final double slopeRad = Math.toRadians(slopeDegsGrid
+                            .getValue(col, row));
+
+                    final double realArea = area / Math.cos(slopeRad);
+
+                    if (!Double.isInfinite(realArea)) {
+                        realAreaGrid.setValue(col, row, realArea);
+                    } else {
+                        realAreaGrid.setValue(col, row,
+                                slopeDegsGrid.getNoData());
+                    }
+                    // hillshadeGrid.setValue(col, row,
+                    // Math.toDegrees(hillshade));
+
+                }
+            }
+        }
+
+        return realAreaGrid;
+
+    }
+
+    private final int stripeEffectiveRows;
+    private final DoubleBasicGrid slopeDegsGrid;
+
+    private final int yOffset;
+
+}
