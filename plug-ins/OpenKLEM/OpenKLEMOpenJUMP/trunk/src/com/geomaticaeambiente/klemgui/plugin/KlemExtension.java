1f8f3d38b74c79e448724469327c631054e0cfd8
ma15569 <ma15569@d6f7b87f-2e33-0410-8384-f77abc8d64da>
2018-12-27 20:56:28 +0000

    Add serial number to OpenKLEM

--- plug-ins/OpenKLEM/OpenKLEMOpenJUMP/trunk/src/com/geomaticaeambiente/klemgui/plugin/KlemExtension.java
+++ plug-ins/OpenKLEM/OpenKLEMOpenJUMP/trunk/src/com/geomaticaeambiente/klemgui/plugin/KlemExtension.java
@@ -9,6 +9,19 @@ import com.vividsolutions.jump.workbench.plugin.PlugInContext;
  */
 public class KlemExtension extends Extension{
 
+    
+    private static final String NAME = "OpenKLEM - raster analisys (Geomatica)";
+    private static final String VERSION = "1.0 (2018-12-27)";
+
+    public String getName() {
+        return NAME;
+    }
+
+    public String getVersion() {
+        return VERSION;
+    }
+    
+    
     @Override
     public void configure(PlugInContext context) throws Exception {
         new StartPlugIn().initialize(context);
